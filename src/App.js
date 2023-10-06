a6b9b53f32ef64129f069722acec1ad6efec0c72
Nicolas Espinoza <niko.ep@gmail.com>
2023-10-07 02:31:54 +0900

    Add counter to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         The version is: {process.env.REACT_APP_VERSION}
       </header>
+      <div>counter: {counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>increment</button>
     </div>
   );
 }
